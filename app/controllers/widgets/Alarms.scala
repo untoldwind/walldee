ba4bee0e1d395bf891242b4ec9689fc89bdc4f7c
Untoldwind <landru@untoldwind.net>
2012-11-14 21:32:49 +0100

    Very simple alarm implementation

--- app/controllers/widgets/Alarms.scala
+++ app/controllers/widgets/Alarms.scala
@@ -6,15 +6,20 @@ import models.{Alarm, DisplayItem, Display}
 import play.api.templates.Html
 import java.security.MessageDigest
 import models.utils.DataDigest
+import org.joda.time.DateTime
 
 object Alarms extends Widget[AlarmsConfig] {
   val configMapping = mapping(
     "labelFont" -> optional(text),
-    "labelSize" -> optional(number)
+    "labelSize" -> optional(number),
+    "descriptionFont" -> optional(text),
+    "descriptionSize" -> optional(number),
+    "alertPeriod" -> optional(number)
   )(AlarmsConfig.apply)(AlarmsConfig.unapply)
 
   def render(display: Display, displayItem: DisplayItem): Html = {
-    views.html.display.widgets.alarms.render(display, displayItem, Alarm.findAllForToday())
+    views.html.display.widgets.alarms.render(display, displayItem,
+      findAllPendingForToday(displayItem.alarmsConfig.flatMap(_.alertPeriod)))
   }
 
   override def etag(display: Display, displayItem: DisplayItem): String = {
@@ -26,12 +31,34 @@ object Alarms extends Widget[AlarmsConfig] {
     dataDigest.update(displayItem.height)
     dataDigest.update(displayItem.widgetConfigJson)
 
-    Alarm.findAllForToday().foreach {
-      alarm =>
+    findAllPendingForToday(displayItem.alarmsConfig.flatMap(_.alertPeriod)).foreach {
+      case (alarm, alert) =>
+        dataDigest.update(alert)
         dataDigest.update(alarm.id)
         dataDigest.update(alarm.name)
         dataDigest.update(alarm.repeatDays)
     }
     dataDigest.base64Digest()
+  }
+
+  private def findAllPendingForToday(alarmPeriod: Option[Int]): Seq[(Alarm, Boolean)] = {
+
+    val alarms = Alarm.findAllForToday()
+    val period = alarmPeriod.getOrElse(5) * 60L * 1000L
+    val now = System.currentTimeMillis()
+
+    alarms.filter(_.nextDate.getTime < now - period).foreach {
+      alarm =>
+        alarm.repeatDays.map {
+          repeatDays =>
+            val nextDate = new DateTime(alarm.nextDate.getTime).plusDays(repeatDays)
+            val newAlarm = Alarm(alarm.id, alarm.name, nextDate.toDate, Some(repeatDays))
+            newAlarm.update
+        }
+    }
+    alarms.filter(_.nextDate.getTime >= now - period).map {
+      alarm =>
+        (alarm, alarm.nextDate.getTime < now + period)
+    }
   }
 }
--- app/models/widgetConfigs/AlarmsConfig.scala
+++ app/models/widgetConfigs/AlarmsConfig.scala
@@ -3,9 +3,13 @@ package models.widgetConfigs
 import play.api.libs.json._
 import play.api.libs.json.JsObject
 import play.api.libs.json.JsString
+import play.api.data.OptionalMapping
 
 case class AlarmsConfig(labelFont: Option[String] = None,
-                       labelSize: Option[Int] = None)
+                        labelSize: Option[Int] = None,
+                        descriptionFont: Option[String] = None,
+                        descriptionSize: Option[Int] = None,
+                        alertPeriod: Option[Int] = None)
 
 object AlarmsConfig {
 
@@ -13,11 +17,17 @@ object AlarmsConfig {
     override def reads(json: JsValue): AlarmsConfig =
       AlarmsConfig(
         (json \ "labelFont").asOpt[String],
-        (json \ "labelSize").asOpt[Int])
+        (json \ "labelSize").asOpt[Int],
+        (json \ "descriptionFont").asOpt[String],
+        (json \ "descriptionSize").asOpt[Int],
+        (json \ "alertPeriod").asOpt[Int])
 
     override def writes(alarmsConfig: AlarmsConfig): JsValue = JsObject(
       alarmsConfig.labelFont.map("labelFont" -> JsString(_)).toSeq ++
-        alarmsConfig.labelSize.map("labelSize" -> JsNumber(_)).toSeq)
+        alarmsConfig.labelSize.map("labelSize" -> JsNumber(_)).toSeq ++
+        alarmsConfig.descriptionFont.map("descriptionFont" -> JsString(_)).toSeq ++
+        alarmsConfig.descriptionSize.map("descriptionSize" -> JsNumber(_)).toSeq ++
+        alarmsConfig.alertPeriod.map("alertPeriod" -> JsNumber(_)).toSeq)
   }
 
 }
