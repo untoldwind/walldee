c2ec2b7c3c298ed32a1f088f1941d81d7ff021f5
Untoldwind <landru@untoldwind.net>
2012-11-20 21:57:46 +0100

    Nicer table layout

--- app/views/utils/package.scala
+++ app/views/utils/package.scala
@@ -2,6 +2,7 @@ package views
 
 import java.util.Date
 import util.matching.Regex
+import play.api.templates.Html
 
 package object utils {
   val dateFormatter = org.joda.time.format.DateTimeFormat.forPattern("yyyy-MM-dd HH:mm")
@@ -18,5 +19,15 @@ package object utils {
             Range.inclusive(1, found.groupCount).map(found.group(_)).mkString
         }
     }.getOrElse(value)
+  }
+
+  def mkTable[A](columns: Int, values: Seq[A])(tmpl: A => Html) = {
+    Range(0, values.length, columns).foldLeft(Html("<tbody>")) {
+      (html, idx) =>
+        values.slice(idx, idx + columns).foldLeft(html + Html("<tr>")) {
+          (html, value) =>
+            html + tmpl(value)
+        }
+    } + Html("</tbody>")
   }
 }
