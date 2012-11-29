e825f767d6b1dc8df12039d2345ae2c13a8199fb
Untoldwind <landru@untoldwind.net>
2012-11-29 20:16:40 +0100

    Made it all configurable

--- app/controllers/widgets/metrics/CoverageDialValueIndicator.scala
+++ app/controllers/widgets/metrics/CoverageDialValueIndicator.scala
@@ -17,25 +17,14 @@ class CoverageDialValueIndicator(font: Font) extends DialValueIndicator {
   val formatter = new DecimalFormat("0.0", DecimalFormatSymbols.getInstance(Locale.US))
 
   override def draw(g2: Graphics2D, plot: DialPlot, frame: Rectangle2D, view: Rectangle2D) {
-    val rect = new Rectangle2D.Double(0.22 * frame.getWidth, 0.13 * frame.getHeight,
-      0.56 * frame.getWidth, 0.22 * frame.getHeight)
     val value = plot.getValue(this.getDatasetIndex)
     val valueStr = if (value >= 100) "100" else formatter.format(value)
-    val fm = g2.getFontMetrics(font.deriveFont(rect.getHeight.toFloat))
-    val bounds = TextUtilities.getTextBounds(valueStr, g2, fm)
-    val factorW = rect.getWidth / bounds.getWidth
-    val factorH = rect.getHeight / -bounds.getY
-
-    val rescaledFont = if (factorW < factorH)
-      font.deriveFont((factorW * rect.getHeight).toFloat)
-    else
-      font.deriveFont((factorH * rect.getHeight).toFloat)
 
     g2.setPaint(this.getPaint)
-    g2.setFont(rescaledFont)
+    g2.setFont(this.getFont)
 
     TextUtilities.drawAlignedString(valueStr, g2,
-      rect.getX.toFloat + rect.getWidth.toFloat / 2, rect.getY.toFloat + rect.getHeight.toFloat,
-      TextAnchor.BASELINE_CENTER)
+      frame.getX.toFloat + frame.getWidth.toFloat / 2, frame.getHeight.toFloat * 0.23f,
+      TextAnchor.CENTER)
   }
 }
