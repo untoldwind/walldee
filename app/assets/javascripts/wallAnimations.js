f83ec10d1319c5c9db96d966c32400e41c0fe505
Untoldwind <landru@untoldwind.net>
2013-03-15 15:28:43 +0100

    Use textfill instead of slabtext

--- app/assets/javascripts/wallAnimations.js
+++ app/assets/javascripts/wallAnimations.js
@@ -1,5 +1,7 @@
 var animations = (function() {
     function animateElements() {
+        $(".bigText").textfill({debug: true, maxFontPixels: 0});
+
         $(".running").fadeTo(500, 0.3).fadeTo(500, 1.0, animateElements);
     }
 
