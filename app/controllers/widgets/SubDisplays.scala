c4033302a59aef5a25d751bc3c3a3b4fc92171de
Untoldwind <landru@untoldwind.net>
2013-04-29 10:29:38 +0200

    Long polling fix (or rather workaround)

--- app/controllers/widgets/SubDisplays.scala
+++ app/controllers/widgets/SubDisplays.scala
@@ -6,17 +6,23 @@ import models.{Display, DisplayItem}
 
 object SubDisplays extends Widget[SubDisplaysConfig] {
   def renderHtml(display: Display, displayItem: DisplayItem): Html = {
+    println(">>>> Render dender " + display)
     (for {
       subDisplayConfig <- displayItem.widgetConfig[SubDisplaysConfig]
       subDisplayId <- subDisplayConfig.displays.headOption.map(_.displayId)
       subDisplay <- Display.findById(subDisplayId)
     } yield {
+      println(">>> Meep")
       val renderedWidgets = DisplayItem.findAllForDisplay(subDisplayId).map {
         subDisplayItem =>
+          println(">>> Meh " + subDisplayItem)
           Widget.forDisplayItem(subDisplayItem).render(subDisplay, subDisplayItem)
       }
-
+      println(">>> go " + renderedWidgets.length)
       views.html.display.widgets.subDisplay(display, displayItem, renderedWidgets)
-    }).getOrElse(Html(""))
+    }).getOrElse {
+      println(">>> buckup")
+      Html("")
+    }
   }
 }
